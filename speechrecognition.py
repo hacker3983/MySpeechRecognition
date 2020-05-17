079568837001dea73af53c9958778ce575ad4e68
hacker3983 <54384337+hacker3983@users.noreply.github.com>
2020-05-17 13:42:47 -0400

    Add files via upload

---
+++ speechrecognition.py
@@ -0,0 +1,51 @@
+import speech_recognition as sr
+from time import ctime
+import webbrowser
+import playsound
+import os
+import random
+from gtts import gTTS
+
+r = sr.Recognizer()
+
+def record_audio(ask = False):
+  with sr.Microphone() as source:
+    if ask:
+      MrHacker_speak(ask)
+    MrHacker_speak('Please say something')
+    audio = r.listen(source)
+    try:
+       voice_data = r.recognize_google(audio)
+       #print(voice_data)
+    except sr.UnknownValueError:
+      MrHacker_speak('Sorry, I did not get that')
+    except sr.RequestError:
+       MrHacker_speak('Sorry, my speech service is down')
+    return voice_data
+
+def MrHacker_speak(audio_string):
+  tts = gTTS(text=audio_string, lang='en')
+  r = random.randint(1, 10000000)
+  audio_file = 'audio' + str(r) + '.mp3'
+  tts.save(audio_file)
+  playsound.playsound(audio_file)
+  print(audio_string, [...])
+  os.remove(audio_file)
+  
+def respond(voice_data):
+  if 'what is your name' in voice_data:
+    MrHacker_speak('My name is Mr Hacker')
+  if 'what is the time' in voice_data:
+    MrHacker_speak(ctime())
+  if 'search' in voice_data:
+    search = record_audio('what do you want to search for?')
+    url = 'https://google.com/search?q=' + search
+    webbrowser.get().open(url)
+    MrHacker_speak('here is what I found for' + search)
+  #if 'find location' in voice_data:
+   # location
+
+MrHacker_speak('How can I help you?')
+voice_data = record_audio()
+respond(voice_data)
+
